a0b96efc2e2ac12c080fba9d5c1164a106a6867d
laxminarasimha-vidooly <54302572+laxminarasimha-vidooly@users.noreply.github.com>
2019-08-20 11:24:51 +0530

    Update and rename Model_2.py to Ad_per_prediction.py

--- Ad_per_prediction.py
+++ Ad_per_prediction.py
@@ -1,11 +1,6 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Jun  6 15:59:00 2019
-
-@author: chiru
-"""
-
-
+#Importing libraries#
+from sklearn.externals import joblib
+from sklearn.metrics import mean_squared_error,r2_score
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt 
@@ -21,92 +16,46 @@ from sklearn.utils import shuffle
 from sklearn import svm
 from datetime import date
 import os
-os.chdir("E:/Laxmi_Rnd/My Laptop/AD_vs_organic_train_updated")
+
+#Importing training data and preparing training data#
 df=pd.read_excel("train_data_2.xlsx")
 df=shuffle(df)
-df.head()
-df.info()
-
 def derive_columns(df):
     
     df['views_c']=df['views']/10000
     df['eng_c']=df['engagement']/500
     df['log_of_v/e']=np.log(df['views']/df['engagement'])
     df['eng_per']=(df['engagement']/df['views'])*100
-#    df['published_date']=df['published_date'].map(lambda x:x.split('T')[0])
-#    df['published_date']=pd.to_datetime(df['published_date'])
-#    d0=date(2019,6,6)
-#    d0=pd.to_datetime(d0)
-#    d1=df['published_date']
-#    delta=d0-d1
-#    df['days']=delta.astype('str').map(lambda x:x.split(' ')[0]).astype('int')
-#    df['views/days']=df['views']/df['days']
-#    df['eng/days']=df['engagement']/df['days']
-#    df['ratio']=df['views/days']/df['eng/days']    
     return df
 
 df=derive_columns(df)
-
-#########################################################################
-# DATA Visualization
-
-
-########  Ratio of views and engagemnet with respect to days
-#
-#plt.figure(figsize=(10,10))
-#sns.scatterplot(x='ratio',y='AD_per',data=df)
-#plt.xlim(0,2000)
-#################   Log value
-#
-#plt.figure(figsize=(10,10))
-#sns.scatterplot(x='Log_v/e',y='AD_per',data=df)
-
-##########################################################################
-
-df.info()
 df_train=df[['log_of_v/e','eng_per','views_c','eng_c','AD_per']]
-df_train.head()
-
-df_train.head()
 y=df_train.pop('AD_per')
 
+#Train/test split#
 x_train,x_test,y_train,y_test=train_test_split(df_train,y,test_size=0.3,random_state=100)
-x_train.head()
-x_test.shape
 
+#Building model#
 folds=KFold(n_splits=2,shuffle=True,random_state=100)
 hyper_param=[{'C':[10,100]}]
 model=svm.SVR(kernel='linear', tol=0.01,epsilon=0.1)
 model_2_cv=GridSearchCV(estimator=model,cv=folds,param_grid=hyper_param,scoring='neg_mean_squared_error',return_train_score=True)
-
-
 model_2_cv.fit(x_train,y_train)
 model_2_cv.best_params_['C']
-
 model_2=svm.SVR(kernel='linear',C=10,gamma='auto',tol=0.01)
-
-model_2.get_params
-
 model_2.fit(x_train,y_train)
 
+#Prediction on test data#
 y_pred_2=model_2.predict(x_test)
-
 c=[i for i in range(len(y_test.index))]
 plt.figure(figsize=(15,10))
 plt.plot(c,y_test)
 plt.plot(c,y_pred_2)
 
-from sklearn.metrics import mean_squared_error,r2_score
+#Calculating error metrics#
 mse_2=mean_squared_error(y_test,y_pred_2)
 print(r2_score(y_test,y_pred_2))
 print(mse_2)
 
-from sklearn.externals import joblib
+#Exporting model#
 joblib.dump(model_2,'model_2.pkl')
-
-
-#x_test_1=df.loc[y_test.index]
-#x_test_1['predict_ad']=y_pred_2
-#x_test_1=x_test_1.reset_index()
-#x_test_1.to_csv("test_ka_train_model_2_2.csv")
-#len(y_test.index)
