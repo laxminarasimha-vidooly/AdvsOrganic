869fa82c51d0c0df92615eddc01bd94917d184a6
laxminarasimha-vidooly <54302572+laxminarasimha-vidooly@users.noreply.github.com>
2019-08-21 19:08:52 +0530

    Update API_ad_organic.py

--- API_ad_organic.py
+++ API_ad_organic.py
@@ -61,7 +61,7 @@ def video_ad_prediction():
          <br>
     </form></h2><div style="background-color: rgb(255, 143, 0); display: none; color: white; text-align: center; position: fixed; top: 0px; left: 0px; width: 100%; height: auto; min-width: 100%; min-height: auto; max-width: 100%; font: 12px &quot;Helvetica Neue&quot;, Helvetica, Arial, Geneva, sans-serif; cursor: pointer; padding: 5px;"><span style="color: white; font: 12px &quot;Helvetica Neue&quot;, Helvetica, Arial, Geneva, sans-serif;">You have turned off the paragraph player. You can turn it on again from the options page.</span><img src="chrome-extension://gfjopfpjmkcfgjpogepmdjmcnihfpokn/img/icons/icon-close_16.png" style="width: 20px; height: auto; min-width: 20px; min-height: auto; max-width: 20px; float: right; margin-right: 10px;"></div></body></html>
 '''
-
+#Mail config for API
 def process(file,email):
     
     current_time='-'.join('_'.join(str(datetime.datetime.now().replace(microsecond=0)).split(' ')).split(':'))
